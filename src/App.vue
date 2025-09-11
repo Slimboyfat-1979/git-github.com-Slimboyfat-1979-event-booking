7c1ed047c08262d03696ce51097d8b004c899ec7
Slimboyfat-1979 <slimboyfat1979@gmail.com>
2025-09-11 21:50:18 +1200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="container mx-auto my-8 space-y-8">
+    <h1 class="text-4xl">Event Booking App</h1>
+    <h2 class="text-2xl font-medium">All Events</h2>
+    <section class="grid grid-cols-2 gap-8">
+      <EventCard @register="register" title="vue conference 2025" when="2025-05-01" description="Conference about Vue and Javascript" v-for="i in 8" :key="i" />
+    </section>
+    <h2 class="text-2xl font-medium">Your Bookings</h2>
+  </main>
+</template>
+
+<script setup>
+import EventCard from '@/components/EventCard.vue';
+
+
+
+</script>

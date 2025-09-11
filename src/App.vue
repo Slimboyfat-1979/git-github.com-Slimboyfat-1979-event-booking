024599ec7792baaba58356b3babd7e266d749a12
Slimboyfat-1979 <slimboyfat1979@gmail.com>
2025-09-11 21:51:41 +1200

    Minor change

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1 class="text-4xl">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
     <section class="grid grid-cols-2 gap-8">
-      <EventCard @register="register" title="vue conference 2025" when="2025-05-01" description="Conference about Vue and Javascript" v-for="i in 8" :key="i" />
+      <EventCard @register="console.log('Registered')" title="vue conference 2025" when="2025-05-01" description="Conference about Vue and Javascript" v-for="i in 8" :key="i" />
     </section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
   </main>

040cfb433b3cbb31370e233e41d5905fae3e74ae
Slimboyfat-1979 <slimboyfat1979@gmail.com>
2025-09-13 19:37:25 +1200

    Slots

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,19 +1,22 @@
 <template>
-    <div class="border border-gray-200 bg-white rounded-md">
-        <h3 class="p-4 font-medium text-xl border-b border-gray-200">{{ title }}</h3>
-        <p class="p-4 border-b border-gray-200">{{ when }}</p>
-        <p class="p-4">{{ description }}</p>
-        <section class="flex justify-end p-4">
-            <button class="text-sm font-medium border border-gray-200 rounded-full px-3 py-1 hover:bg-gray-100" @click="$emit('register')">Register</button>
-        </section>
-    </div>
+  <div class="border border-gray-200 bg-white rounded-md">
+    <h3 class="p-4 font-medium text-xl border-b border-gray-200">{{ title }}</h3>
+    <p class="p-4 border-b border-gray-200">{{ when }}</p>
+    <p class="p-4">{{ description }}</p>
+    <section class="flex justify-end p-4">
+      <RoundButton @click="$emit('register')">
+        <pre>Register</pre>
+      </RoundButton>
+    </section>
+  </div>
 </template>
 
 <script setup>
+import RoundButton from './RoundButton.vue';
 defineProps({
-    title: String,
-    when: String,
-    description: String
-})
-defineEmits(['register'])
+  title: String,
+  when: String,
+  description: String,
+});
+defineEmits(['register']);
 </script>

ad014ccc818118ac8da574b14bc1053843b8ad2c
Slimboyfat-1979 <slimboyfat1979@gmail.com>
2025-09-14 20:10:37 +1200

    Fetching events from jsons server

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,34 @@
     <h1 class="text-4xl">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
     <section class="grid grid-cols-2 gap-8">
-      <EventCard  @register="console.log('Registered')" title="vue conference 2025" when="2025-05-01" description="Conference about Vue and Javascript" v-for="i in 8" :key="i" />
+      <EventCard
+        v-for="event in events"
+        @register="console.log('Registered')"
+        :title="event.title"
+        :when="event.date"
+        :description="event.description"
+        :key="event.id"
+      />
     </section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
     <section class="grid grid-cols-1 gap-8">
-    <BookingItem v-for="i in 3" :key="i"></BookingItem>
-
+      <BookingItem v-for="i in 3" :key="i"></BookingItem>
     </section>
   </main>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue';
 import EventCard from '@/components/EventCard.vue';
 import BookingItem from './components/BookingItem.vue';
 
+const events = ref([]);
 
+const fethEvents = async () => {
+  const response = await fetch('http://localhost:3001/events');
+  events.value = await response.json();
+  console.log(events.value)
+};
 
+onMounted(() => fethEvents())
 </script>
